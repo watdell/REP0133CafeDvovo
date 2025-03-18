551c5a86b7660b1f00fccf16b4fd2f89e500a92d
Adrian <adrian13smith@gmail.com>
2025-03-18 13:37:31 -0300

    uhm

--- public/comercial/caixa/search.php
+++ public/comercial/caixa/search.php
@@ -5,33 +5,8 @@
     $d = strval($_GET['d']);
     $p = strval($_GET['p']);
 
-    function dbConnection() {
-        // Ativar relatórios de erro
-        error_reporting(E_ALL);
-        ini_set('display_errors', 0); // Desativa a exibição de erros no navegador (em produção, geralmente deve ser 0)
-    
-        // Configurações do banco de dados
-        $host = 'localhost'; // Endereço do servidor
-        $port = 3306;        // Porta do MySQL do XAMPP (verifique no painel de controle do XAMPP)
-        $user = 'root';     // Nome de usuário do MySQL
-        $password = ''; // Senha do MySQL
-        $database = 'cafedvovo'; // Nome do banco de dados
-    
-        // Criando a conexão com o banco de dados
-        $conn = new mysqli($host, $user, $password, $database, $port);
-    
-        // Definindo a codificação UTF-8 para aceitar caracteres especiais
-        $conn->set_charset("utf8");
-    
-        // Verificando se a conexão foi bem-sucedida
-        if ($conn->connect_error) {
-            die("Conexão falhou: " . $conn->connect_error);
-        }
-    
-        // Retornando a conexão para ser usada nas outras funções ou páginas
-        return $conn;
-    }
-    
+    include '../../../serverside/config/dbConnection.php';
+
     $conn = dbConnection();
 
     if ($c == 'id') {
@@ -161,5 +136,8 @@
         </div><hr>";
     }
 }
+
+    $stmt->close();
+    $conn->close();
 
     mysqli_close($conn);
--- public/relatorios/produtos/produto_search.php
+++ public/relatorios/produtos/produto_search.php
@@ -2,33 +2,8 @@
     $q = strval($_GET['q']);
     $tipo = strval($_GET['tipo']);
 
-    function dbConnection() {
-        // Ativar relatórios de erro
-        error_reporting(E_ALL);
-        ini_set('display_errors', 0); // Desativa a exibição de erros no navegador (em produção, geralmente deve ser 0)
-    
-        // Configurações do banco de dados
-        $host = 'localhost'; // Endereço do servidor
-        $port = 3306;        // Porta do MySQL do XAMPP (verifique no painel de controle do XAMPP)
-        $user = 'root';     // Nome de usuário do MySQL
-        $password = ''; // Senha do MySQL
-        $database = 'cafedvovo'; // Nome do banco de dados
-    
-        // Criando a conexão com o banco de dados
-        $conn = new mysqli($host, $user, $password, $database, $port);
-    
-        // Definindo a codificação UTF-8 para aceitar caracteres especiais
-        $conn->set_charset("utf8");
-    
-        // Verificando se a conexão foi bem-sucedida
-        if ($conn->connect_error) {
-            die("Conexão falhou: " . $conn->connect_error);
-        }
-    
-        // Retornando a conexão para ser usada nas outras funções ou páginas
-        return $conn;
-    }
-    
+    include '../../../serverside/config/dbConnection.php';
+
     $conn = dbConnection();
 
     $sql = "SELECT * FROM produto WHERE nome LIKE '%" . $q . "%'";
@@ -86,5 +61,8 @@
 
         }
     }
+
+    $stmt->close();
+    $conn->close();
 
     mysqli_close($conn);
